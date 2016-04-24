dd10e98332ae8390b1769182ff064d54274fa145
DXsmiley <dxsmiley@hotmail.com>
2016-04-25 09:59:34 +1000

    Package Data

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+# Consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding = 'utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name = 'edgy-json',
+	version = '0.0.1',
+	url = 'https://github.com/DXsmiley/edgy-json',
+	author = 'DXsmiley',
+	author_email = 'dxsmiley@hotmail.com',
+	license = 'MIT',
+
+	description = 'Verify JSON like a hipster.',
+	long_description = long_description,
+
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+
+		'Intended Audience :: Developers',
+
+		'License :: OSI Approved :: MIT License',
+
+		'Programming Language :: Python :: 3',
+	],
+
+	keywords = 'validate verify json',
+
+	py_modules = ["edgy"]
+)
