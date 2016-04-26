1424f1e31861f11c0f3d5681bd995573d31f8aea
DXsmiley <dxsmiley@hotmail.com>
2016-04-26 11:02:17 +1000

    Version 0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='edgy-json',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/DXsmiley/edgy-json',
     author='DXsmiley',
     author_email='dxsmiley@hotmail.com',
