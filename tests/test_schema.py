ea2290e47b3b87c00f1a6083e287eff283585914
DXsmiley <dxsmiley@hotmail.com>
2016-05-01 21:27:56 +1000

    Schema that describes schemas

---
+++ tests/test_schema.py
@@ -0,0 +1,74 @@
+import edgy
+
+
+SCHEMA = \
+{
+    '__named__':
+    {
+        'schema':
+        [
+            'string',
+            True,
+            False,
+            {
+                '__type__': 'list',
+                '__item__': '@schema'
+            },
+            {
+                '~__named__': ['none', '@named'],
+                '~__this__': ['none', '@schema'],
+                '__extra__': 'anything',
+                '__this__':
+                [
+                    {
+                        '~__named__': 'anything',
+                        '~__this__': 'anything',
+                        '~__type__': ['none', 'object'],
+                        '__extra__': '@schema'
+                    },
+                    {
+                        '~__named__': 'anything',
+                        '~__this__': 'anything',
+                        '~__type__':
+                        {
+                            '__type__': 'string',
+                            'equals': 'int'
+                        },
+                        'equals': ['none', 'int'],
+                        'minimum': ['none', 'int'],
+                        'maximum': ['none', 'int']
+                    },
+                    {
+                        '~__named__': 'anything',
+                        '~__this__': 'anything',
+                        '~__type__':
+                        {
+                            '__type__': 'string',
+                            'equals': 'string'
+                        },
+                        'equals': ['none', 'int'],
+                    },
+                    {
+                        '~__named__': 'anything',
+                        '~__this__': 'anything',
+                        '~__type__':
+                        {
+                            '__type__': 'string',
+                            'equals': 'list'
+                        },
+                        '~__item__': '@schema'
+                    }
+                ]
+            }
+        ],
+        'named':
+        {
+            '__extra__': '@schema'
+        }
+    },
+    '__this__': '@schema'
+}
+
+
+def test_schema():
+    assert edgy.check(SCHEMA, SCHEMA)
